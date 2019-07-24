2b2fa9b11a0a36a8b9db85648bc08eb4aea2be1e
Maysam Taherian <maysamtaherian@Maysams-MacBook-Pro.local>
2019-07-23 20:57:25 -0500

    collaborative coding session1 completed

--- Controllers/db.js
+++ Controllers/db.js
@@ -1,14 +1,1 @@
-const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/' + 'TravelToo';
-const db = mongoose.connection;
-
-//middleware
-mongoose.connect(mongoURI, { useNewUrlParser: true}, () =>{
-    console.log("The connection works")
-});
-
-//errorlog
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', mongoURI));
-db.on('disconnected', () => console.log('mongo disconnected'));
